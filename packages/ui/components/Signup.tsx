547b6cb6fb207ee06fb21af0fa2394ad4b98c214
Mishragini <mishragini11.com>
2023-11-16 11:24:40 +0530

    first commit

---
+++ packages/ui/components/Signup.tsx
@@ -0,0 +1,9 @@
+export const Signup=()=>{
+return(
+    <>
+    <input type="email" placeholder="email"></input>
+    <input type="password" placeholder="password"></input>
+    <button>Submit</button>
+    </>
+)
+}
